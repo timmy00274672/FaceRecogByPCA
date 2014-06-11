209ac13568d33e412b8617ebb978120850736a35
timmy00274672 <timmy00274672@gmail.com>
2014-06-11 12:59:45 +0800

    Create the API

---
+++ facerecog.m
@@ -0,0 +1,15 @@
+%% Copyright (c) 2014, Kalyan S Dash
+%% I adjust the source code from http://www.mathworks.com/matlabcentral/fileexchange/45750-face-recognition-using-pca/content/facerecog.m
+
+function [ recognized_img ] = facerecog( trainingDataPathList, testImgPath )
+
+%{
+	Using 
+	@param trainingDataPathList : it's array of training data path
+	@param testImgPath : it's test data img path
+	@return recognized_img : in path format	
+%}
+
+
+end
+
