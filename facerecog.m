f7bfd5b06e126cc0b50a90b79a354eb4adb7e25f
timmy00274672 <timmy00274672@gmail.com>
2014-06-11 14:55:19 +0800

    complete the first version

--- facerecog.m
+++ facerecog.m
@@ -1,16 +1,82 @@
 %% Copyright (c) 2014, Kalyan S Dash
 %% I adjust the source code from http://www.mathworks.com/matlabcentral/fileexchange/45750-face-recognition-using-pca/content/facerecog.m
 
-function [ recognized_img ] = facerecog( trainingDataPathList, testImgPath )
-
 %{
-	Using 
+	Using PCA method to classify.
 	@param trainingDataPathList : it's array of training data path
 	@param testImgPath : it's test data img path
 	@return recognized_img : in path format	
+	@assumption all the imgs are in the same size
 %}
+function [ recognized_img ] = facerecog( trainingDataPathList, testImgPath )
+% to simplify to calculation : use gray img instead of rgb
+imgcount = size(trainingDataPathList , 1);
+X = []; %% N feature vector (p dim) in p * N
+for i = 1 : imgcount
+   X = [X , imreadOneD( trainingDataPathList(i,: ))];
+end
+
+m = mean(X,2); % compute the mean of feature vectors
+% substract each vector in X with mean in A
+A = []; 
+for i = 1 : imgcount
+    A = [A double(X(:,i))-m ];
+end
+
+L = A' * A;
+[V,D] = eig(L);
+
+%Select the eigenvector only corresponding eigenvalue > 1
+L_eig_vec = [];
+for i = 1 : size(V,2);
+   if(D(i,i) > 1)
+       L_eig_vec = [L_eig_vec V(:,i)];
+   end
+end
+
+eigenfaces = A * L_eig_vec;
+
+projectimg = [];
+for i = 1 : size(eigenfaces,2)
+   projectimg = [projectimg eigenfaces' * A(:,i)];
+end
+
+testimg = imreadOneD(testImgPath);
+testimg = double(testimg) - m ; %substract with mean from the training vectors
+projtestimg = eigenfaces' * testimg;
+
+[classto, error] = getNearestIndex(projectimg, projtestimg)
+figure;
+subplot(1,2,1), subimage(imread(trainingDataPathList(classto,:))), title('classto')
+subplot(1,2,2), subimage(imread(testImgPath)),title('test img')
+end
+
+%% imreadOneD: function description
+function [outputs] = imreadOneD(imgPath)
+	img = imreadGray(imgPath);
+	[r c] = size(img);
+	outputs = reshape(img',r*c,1);
+end
+
+
+%% imreadGray: function description
+function [img] = imreadGray(imgPath)
+	img = imread(imgPath);
+	if (size(img,3) == 3)
+		img = rgb2gray(img);
+	end
+
+end
 
 
 
+%% getNearestIndex: function description
+function [index error] = getNearestIndex(projectimg, referece)
+	euclide_dist = [];
+	for i = 1 : size(projectimg,2)	
+		euclide_dist = [euclide_dist (norm(projectimg(:,i)-referece))^2];
+	end
+
+	[error index] = min(euclide_dist);
 end
 
--- getNodes.m
+++ getNodes.m
@@ -2,6 +2,13 @@ function [ list ] = getNodes( path )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
+D = dir(path);
+list = [];
+for i = 1 : size(D,1)
+	if not ( strcmp(D(i).name, '.') |  strcmp(D(i).name, '..') | strcmp(D(i).name, 'Thumbs.db') )
+		list = [list;strcat(path,'\',D(i).name)];
+	end		
+end
 
 end
 
